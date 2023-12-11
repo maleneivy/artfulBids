d96036f87ce0bf9922dffd0c9a84b518f7f0a7fc
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-12-11 11:41:48 +0100

    Added CTA for create new listing and log out

--- src/js/pages/myProfile.js
+++ src/js/pages/myProfile.js
@@ -1,3 +1,26 @@
 import { createHeader } from "../components/header.js";
+import { userName, email, credits } from "../utils/storage.mjs";
 
 createHeader();
+
+function createProfile() {
+  const userInfoContainer = document.querySelector(".user-info-container");
+
+  const greetingMessage = document.createElement("h2");
+  greetingMessage.textContent = `Hello ${userName}`;
+
+  const presentEmail = document.createElement("h5");
+  presentEmail.textContent = `${email}`;
+
+  userInfoContainer.appendChild(greetingMessage);
+  userInfoContainer.appendChild(presentEmail);
+
+  const creditsContainer = document.querySelector(".credits-container");
+
+  const presentCredits = document.createElement("p");
+  presentCredits.textContent = `Credits: ${credits}`;
+
+  creditsContainer.appendChild(presentCredits);
+}
+
+createProfile();
