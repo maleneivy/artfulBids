5450d5585c4d660359e44a3347b65b8386e452fd
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-12-13 01:56:42 +0100

    Added api info on my profile page

--- src/js/pages/myProfile.js
+++ src/js/pages/myProfile.js
@@ -1,11 +1,38 @@
+import { API_BASE_URL } from "../api/baseUrl.js";
 import { createHeader } from "../components/header.js";
 import logOutButton from "../components/logOutButton.mjs";
-import { userName, email, credits } from "../utils/storage.mjs";
+import { userName, email, credits, token } from "../utils/storage.mjs";
 
 createHeader();
 logOutButton();
 
-function createProfile() {
+async function getUserData() {
+  try {
+    const getUserProfileUrl = `${API_BASE_URL}/auction/profiles/${userName}?_listings=true`;
+
+    const getData = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await fetch(getUserProfileUrl, getData);
+    const json = await response.json();
+    console.log(response);
+    console.log(json);
+
+    const userCountListings = json._count.listings;
+
+    createProfile(userCountListings);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getUserData();
+
+function createProfile(userCountListings) {
   const userInfoContainer = document.querySelector(".user-info-container");
 
   const greetingMessage = document.createElement("h2");
@@ -13,6 +40,9 @@ function createProfile() {
 
   const presentEmail = document.createElement("h5");
   presentEmail.textContent = `${email}`;
+
+  const presentTotalListings = document.createElement("p");
+  presentTotalListings.textContent = `Total Listings: ${userCountListings}`;
 
   userInfoContainer.appendChild(greetingMessage);
   userInfoContainer.appendChild(presentEmail);
@@ -23,6 +53,5 @@ function createProfile() {
   presentCredits.textContent = `Credits: ${credits}`;
 
   creditsContainer.appendChild(presentCredits);
+  userInfoContainer.appendChild(presentTotalListings);
 }
-
-createProfile();
