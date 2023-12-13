339090af0ad791e7af791430d992afadd7d939b7
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-12-13 15:12:54 +0100

    Added myListings on myProfile

--- src/js/pages/myProfile.js
+++ src/js/pages/myProfile.js
@@ -1,6 +1,7 @@
 import { API_BASE_URL } from "../api/baseUrl.js";
 import { createHeader } from "../components/header.js";
 import logOutButton from "../components/logOutButton.mjs";
+import { formatDateTimeNorwegian } from "../utils/norwegianTimeFormat.mjs";
 import { userName, email, credits, token } from "../utils/storage.mjs";
 
 createHeader();
@@ -8,7 +9,7 @@ logOutButton();
 
 async function getUserData() {
   try {
-    const getUserProfileUrl = `${API_BASE_URL}/auction/profiles/${userName}?_listings=true`;
+    const getUserProfileUrl = `${API_BASE_URL}/auction/profiles/${userName}?_listings=true&_bids=true`;
 
     const getData = {
       method: "GET",
@@ -24,15 +25,17 @@ async function getUserData() {
     console.log(json);
 
     const userCountListings = json._count.listings;
+    const userListings = json.listings;
 
-    createProfile(userCountListings);
+    console.log(userListings);
+    createProfile(userCountListings, userListings);
   } catch (error) {
     console.log(error);
   }
 }
 getUserData();
 
-function createProfile(userCountListings) {
+function createProfile(userCountListings, userListings) {
   const userInfoContainer = document.querySelector(".user-info-container");
 
   const greetingMessage = document.createElement("h2");
@@ -54,4 +57,91 @@ function createProfile(userCountListings) {
 
   creditsContainer.appendChild(presentCredits);
   userInfoContainer.appendChild(presentTotalListings);
+
+  userListings.forEach((listing) => {
+    console.log(listing);
+
+    const listingsContainer = document.querySelector(".listings-container");
+
+    const title = listing.title;
+    let media = listing.media;
+    media = media.toString();
+    const tags = listing.tags;
+    const endsAt = formatDateTimeNorwegian(listing.endsAt);
+    const id = listing.id;
+
+    //ListingCard (Card will link href to the specific listing)
+    const listingCard = document.createElement("a");
+    listingCard.href = `/html/specific-listing.html?id=${id}`;
+    listingCard.classList.add(
+      "card",
+      "col-sm-12",
+      "col-xs-2",
+      "col-md-5",
+      "m-2",
+      "p-0",
+    );
+
+    // Body of the card
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body", "p-0");
+
+    // Image for the cardBody
+    let listingImage = document.createElement("img");
+    listingImage.classList.add("card-img-top");
+
+    if (!media) {
+      listingImage.src = "../../../images/default/default-post-image.jpg";
+    } else {
+      // Split the media string into an array of image URLs
+      const mediaUrls = media.split(",");
+
+      // Select the first image from the array
+      const firstImageUrl = mediaUrls[0].trim();
+
+      // Set the src attribute for the listingImage
+      listingImage.src = firstImageUrl;
+
+      // Handle error for the first image
+      listingImage.onerror = function () {
+        console.log(`Error loading image: ${firstImageUrl}`);
+        // set the default image in case of error
+        listingImage.src = "../../../images/default/default-post-image.jpg";
+        // Remove the onerror handler to prevent the default error message
+        listingImage.onerror = null;
+      };
+    }
+
+    // Title for the Cardbody
+    let listingTitle = document.createElement("h5");
+    listingTitle.classList.add("card-title", "p-2");
+    listingTitle.textContent = title;
+
+    // Categories/tags for the listing
+    let listingTags = document.createElement("p");
+    listingTags.classList.add("card-text", "p-2");
+    if (tags.length === 0) {
+      listingTags.textContent = `No tags`;
+    } else {
+      listingTags.textContent = tags;
+    }
+
+    // Ending time for listing
+    let listingEndsAt = document.createElement("p");
+    listingEndsAt.classList.add("card-text", "p-2");
+    listingEndsAt.textContent = endsAt;
+    listingEndsAt.style.whiteSpace = "pre-line";
+
+    // Append the elements to the cardBody
+    cardBody.appendChild(listingImage);
+    cardBody.appendChild(listingTitle);
+    cardBody.appendChild(listingTags);
+    cardBody.appendChild(listingEndsAt);
+
+    //Append the cardBody to the listingCard
+    listingCard.appendChild(cardBody);
+
+    // Append the listingCard to its container
+    listingsContainer.appendChild(listingCard);
+  });
 }
