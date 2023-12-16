e356561bacf60b23262d8cc4909d5091993829b8
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-12-16 14:53:08 +0100

    Created deleteListing.js

---
+++ src/js/components/deleteListing.mjs
@@ -0,0 +1,41 @@
+import { API_BASE_URL } from "../api/baseUrl.js";
+import { displayMessage } from "../utils/displayMessage.mjs";
+import { token } from "../utils/storage.mjs";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const listingId = urlParams.get("id");
+const deleteButton = document.querySelector("#delete-listing-button");
+
+async function deleteListing() {
+  const url = `${API_BASE_URL}/auction/listings/${listingId}`;
+
+  const deleteOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ id: listingId }),
+  };
+
+  try {
+    const response = await fetch(url, deleteOptions);
+    // const json = await response.json();
+
+    if (response.ok) {
+      window.location.href = "/html/my-profile.html";
+      alert(`Your listing was successfully deleted`);
+    } else {
+      displayMessage("error-message", `Something went wrong`, ".edit-message");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+deleteButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  deleteListing(listingId);
+});
